98073032dae50885b063e640083cfd8ab3f40661
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-16 16:55:25 +0300

    reorder imports and update default theme in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
 
 const fontSans = Inter({
   variable: "--font-sans",
