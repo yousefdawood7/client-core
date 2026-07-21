0ce52820e514056990fe9989f682bdded21f068a
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-21 16:15:16 +0300

    fix: correct authentication condition in GET endpoint for companies

--- src/features/companies/services/getAllCompanies.ts
+++ src/features/companies/services/getAllCompanies.ts
@@ -1,15 +1,41 @@
 import { prisma } from "@/lib/prisma";
 
 export async function getAllCompanies() {
-  const companies = await prisma.company.findMany({
-    include: {
-      _count: {
-        select: {
-          leads: true,
+  const companies = (
+    await prisma.company.findMany({
+      omit: {
+        userId: true,
+        updatedAt: true,
+      },
+
+      include: {
+        users: {
+          where: {
+            user: {
+              role: "sales",
+            },
+          },
+          select: {
+            user: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+
+        _count: {
+          select: {
+            leads: true,
+          },
         },
       },
-    },
-  });
+    })
+  ).map(({ users, _count, ...company }) => ({
+    ...company,
+    numberOfLeads: _count.leads,
+    salesManager: users[0].user.name,
+  }));
 
   return companies;
 }
