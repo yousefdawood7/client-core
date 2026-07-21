fbda629fb8a1e5e27f78db706fb60c859a0ebd1a
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-21 17:47:52 +0300

    feat: implement handleResponse utility with success and error handling functions

---
+++ src/lib/better-auth/handleResponse.ts
@@ -0,0 +1,44 @@
+type HandleSuccessResponse = {
+  statusCode: number;
+  message?: string;
+  data?: Record<string, unknown>;
+};
+
+type HandleErrorResponse = {
+  statusCode: number;
+  message: string;
+  details?: Record<string, unknown>;
+};
+
+export function handleSuccessResponse({
+  statusCode,
+  message,
+  data,
+}: HandleSuccessResponse) {
+  return Response.json(
+    {
+      statusCode,
+      status: "success",
+
+      ...(message ? { message } : {}),
+      ...(data && { data }),
+    },
+    { status: statusCode },
+  );
+}
+
+export function handleErrorResponse({
+  statusCode,
+  message,
+  details,
+}: HandleErrorResponse) {
+  return Response.json(
+    {
+      statusCode,
+      message,
+      status: statusCode < 500 ? "fail" : "error",
+      ...(details && { details }),
+    },
+    { status: statusCode },
+  );
+}
