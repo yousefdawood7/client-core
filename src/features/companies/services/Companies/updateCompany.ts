a10ee2d5a9eef0dfd1aa9f9aedfa8c564143cce1
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-22 16:30:29 +0300

    fix: format updateCompany function for improved readability

--- src/features/companies/services/Companies/updateCompany.ts
+++ src/features/companies/services/Companies/updateCompany.ts
@@ -2,23 +2,21 @@ import { companyPipeline } from "@/features/companies/utils/company.pipeline";
 import { prisma } from "@/lib/prisma";
 
 export async function updateCompany(
-    id: string,
-    data: Partial<{ name: string; url: string }>
+  id: string,
+  data: Partial<{ name: string; url: string }>,
 ) {
-    const company = await prisma.company.update({
-        where: { id },
-        data,
-        ...companyPipeline,
-    });
+  const company = await prisma.company.update({
+    where: { id },
+    data,
+    ...companyPipeline,
+  });
 
-    return (
-        company && {
-            id: company.id,
-            name: company.name,
-            url: company.url,
-            createdAt: company.createdAt,
-            numberOfLeads: company._count?.leads ?? 0,
-            salesManager: company.users?.[0]?.user?.name ?? null,
-        }
-    );
+  return {
+    id: company.id,
+    name: company.name,
+    url: company.url,
+    createdAt: company.createdAt,
+    numberOfLeads: company._count?.leads ?? 0,
+    salesManager: company.users?.[0]?.user?.name ?? null,
+  };
 }
